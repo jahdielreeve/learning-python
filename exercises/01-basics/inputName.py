610ac231c8a7225a00d27874c147092a0096f683
ylongtay <yl0ng.ted@gmail.com>
2025-11-24 00:27:14 +0800

    python chapter 1 exercises

---
+++ exercises/01-basics/inputName.py
@@ -0,0 +1,6 @@
+print('What is your first name?') #Ask for their first name.
+my_firstName = input('>')
+print('What is your last name?') #Ask for their last name.
+_last = input ('>')
+print('Nice to meet you, ' + my_firstName + ' ' + _last + '!')
+print('Your name has ' + str(len(my_firstName+_last)) + ' letters.')
